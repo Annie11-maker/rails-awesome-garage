454b2a73e90348078c875ee85b677cf81d468781
quratulain shouket <quratulainshouket11@gmail.com>
2025-01-17 09:35:30 +0000

    awesome garage 17/01 9.35am

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,7 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :owner
 
-  # has_many :favourites, dependant: :destroy
+  has_many :favourites
   has_many :reviews
 
   validates :brand, presence: true
